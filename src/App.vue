25aa804b3b460ac179a124ecd192360e56fa2df6
Valentin Galerne <valentin.galerne@gmail.com>
2019-10-23 12:57:36 +0200

    Ajout de 3 inputs + mise en place de fontawesome

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <!-- <div id="app">
+  <div id="nav">
+  <router-link to="/">Home</router-link> |
+  <router-link to="/about">About</router-link>
+</div>
+<router-view/>
+</div> -->
+<form id="main" v-cloak>
+  <div class="wrapper">
+    <h2>Bienvenue sur l’app questionnaire </h2>
+    <i class="fas fa-user-circle"></i><input type="text" placeholder=  "Prénom" /><br/>
+    <i class="fas fa-user-circle"></i><input type="text" placeholder="Nom" /><br/>
+    <input type="text" placeholder="Société" /><br/>
+    <div class="box a">A</div>
+    <div class="box b ">B</div>
+    <div class="box c">C</div>
+    <div class="box d">D</div>
   </div>
+</form>
 </template>
 
 <style lang="scss">
@@ -15,6 +27,36 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.wrapper {
+  display: grip;
+  grid-template-columns: 200px 1fr;
+  grid-template-rows: 120px 135px 100px;
+  grid-gap: 10px;
+  background-color: #fff;
+}
+
+.box {
+  background-color: #444;
+  color: #fff;
+  border-radius: 5px;
+  /*padding: 20px;*/
+  font-size: 150%;
+}
+
+.a {
+  grid-column-start: 2;
+  grid-column-end: 2;
+  grid-row-start: 2;
+  grid-row-end: 2;
+}
+
+.d {
+  grid-column-start: 2;
+  grid-column-end: 2;
+  grid-row-start: 3;
+  grid-row-end: 3;
 }
 
 #nav {
